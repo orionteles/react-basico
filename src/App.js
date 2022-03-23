89f19bba6220cecdd240a587da4c93f974a42766
Orion Teles de Mesquita <orionteles@gmail.com>
2022-03-22 22:16:44 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./components/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <h1>Hello World</h1>
+
+      <Button variant="outline-primary">Primary</Button>
+
     </div>
   );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        
+        <ul>
+            <li><a href="">Página 1</a></li>
+            <li><a href="">Página 2</a></li>
+            <li><a href="">Página 3</a></li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Menu
